c7eaf9ce51c98d5f54e5334820df83d8271f7731
benb05 <benjaminbelotser@gmail.com>
2021-12-08 17:27:10 -0500

    hi

---
+++ gitBasics.java
@@ -0,0 +1,24 @@
+public class gitBasics {
+    public static void main(String[] args){
+        pow2();
+    }
+        public static int pow2(){
+            int i;
+            for (i = 1; i <= 694;){
+                
+                System.out.println(i);
+
+                i*= 2;
+                
+            
+                
+             
+
+
+
+            }
+        return i;
+
+        }
+    
+}
