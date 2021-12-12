7f43e88d94eaede169cb5ab8ae260d56f193332e
benb05 <benjaminbelotser@gmail.com>
2021-12-12 09:22:21 -0500

    hi

---
+++ Arrays.java
@@ -0,0 +1,28 @@
+public class Arrays {
+    
+   
+    private static void displayArray(){
+        int[] array = {1, 2, 3, 4, 5, 7};
+        
+        for(int i = 0; i < array.length; i++){
+
+            System.out.println(array[i]);
+
+            ;
+        }
+    }
+
+    public static void main(String[] args){
+
+        displayArray();
+    }
+        
+      
+
+        
+
+    
+
+        
+    
+}
