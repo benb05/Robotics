ecdb4ebc129015206a320cc2c1751c0ac1f94a57
benb05 <benjaminbelotser@gmail.com>
2021-12-12 22:48:33 -0500

    hi

---
+++ foxArray.java
@@ -0,0 +1,14 @@
+public class foxArray {
+  
+  private static void showFox(){
+      
+    String [] fox = {"h", "e", "l", "l", "o"};
+
+    for (int i = 0; i < fox.length; i++){
+        System.out.print(fox[i]);
+    }
+  }
+    public static void main(String[] args){
+        showFox();
+    }
+}
