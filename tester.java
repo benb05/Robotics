046847a96ce3ef5115d9e4cee0b4ef045d85c2fd
benb05 <benjaminbelotser@gmail.com>
2022-01-12 17:34:58 -0500

    hi

--- tester.java
+++ tester.java
@@ -1,19 +1,29 @@
+
+
 public class tester {
 
 
-    int[] array;
+ private static double[] array = new double[5];
 
-    public void populator(){
+    public static void populator(){
+        
 
-        for (int i = 0; i < array.length; i++){
-            array[i] = math.random *100;
+        for (int i = 0; i < array.length;){
+            array[i] = Math.random() *100;
+            System.out.println(array[i]);
+            i++;
+            
         }
     }
+    
+
+
 
 
 
     public static void main(String[] args) {
-    
+      
+    populator();
 }
 
 
