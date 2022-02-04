168435f55e4f8816983b0055d00e4f34b7a7f944
benb05 <benjaminbelotser@gmail.com>
2022-02-04 16:38:46 -0500

    hi

--- averageFinder.java
+++ averageFinder.java
@@ -1,3 +1,30 @@
 public class averageFinder {
+    //purpose of class is to find the average of an array
+
+    private static int[] arrayPopulator(){
+        int[] targetArray;
+
+        targetArray = new int [10];
+
+        for(int i = 0; i <= targetArray.length; i++){
+            
+            targetArray[i] = (int)Math.random();
+
+
+        }
+        for(int i = 0; i < targetArray.length - 1; i++){
+            System.out.println(targetArray[i]);
+        }
+       return targetArray;
+        }
+
+        
     
+        
+ public static void main(String[] args) {
+     arrayPopulator();
+ }
 }
+        
+    
+
