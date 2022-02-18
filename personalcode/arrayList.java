37f3b61f5d066259121e43b4cbf0f98757d9702a
benb05 <benjaminbelotser@gmail.com>
2022-02-17 22:55:19 -0500

    ArrayList

--- personalcode/arrayList.java
+++ personalcode/arrayList.java
@@ -3,7 +3,26 @@ import java.util.ArrayList;
 
 public class arrayList {
     ArrayList <Integer> deck = new ArrayList <Integer> ();
+    
+    private ArrayList <Integer> arrayPopulator(int size){
+        for (int i = 0; i < size; i++){
+            deck.add((int)(Math.random()* 100));
 
-    public
 
+        }
+        return deck;
+    }
+
+   private void showArray(){
+       arrayPopulator(10);
+    for (int k = 0; k < deck.size(); k ++){
+            System.out.println(deck.get(k));
+       }
+
+   } 
+
+
+   public static void main(String[] args) {
+       new arrayList().showArray();
+   }
 }
